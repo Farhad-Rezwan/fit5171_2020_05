8712093e88920e8722c28287b86d623c721dd790
zkan0005 <54242356+zkan0005@users.noreply.github.com>
2020-04-12 15:46:44 +1000

    Add musicianInstrumentTest

---
+++ fit5171_2020_05-branchKzb/assignment1/src/main/java/allaboutecm/model/Musician.java
@@ -0,0 +1,63 @@
+package allaboutecm.model;
+
+import com.google.common.collect.Sets;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.Set;
+
+import static org.apache.commons.lang3.Validate.notBlank;
+import static org.apache.commons.lang3.Validate.notNull;
+
+/**
+ * An artist that has been featured in (at least) one ECM record.
+ *
+ * See {@https://www.ecmrecords.com/artists/1435045745}
+ */
+public class Musician extends Entity {
+    private String name;
+
+    private URL musicianUrl;
+
+    private Set<Album> albums;
+
+    public Musician(String name) {
+        this.name = name;
+        this.musicianUrl = null;
+
+        albums = Sets.newLinkedHashSet();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Set<Album> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(Set<Album> albums) {
+        this.albums = albums;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Musician that = (Musician) o;
+        return Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
+    }
+
+    public URL getMusicianUrl() {
+        return musicianUrl;
+    }
+
+    public void setMusicianUrl(URL musicianUrl) {
+        this.musicianUrl = musicianUrl;
+    }
+}
